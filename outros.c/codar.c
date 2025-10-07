8462981b31c15e2304e7da661b83718ae8a10ed2
Hendryl-Almeida <dealmeidahendryl6@gmail.com>
2025-10-07 16:24:52 +0000

    muitas adições

---
+++ outros.c/codar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+ int numero;
+
+ do { printf("digite um número pár para sair do programa \n");
+scanf("%d", &numero);
+if (numero % 2 == 0)
+{
+printf("%d é par! \n", &numero");
+} 
+else { printf("%d é ímpar! \n", &numero);
+}
+
+} while (numero % 2 != 0);
+ Printf("Você digitou um número par, saindo do programa..");
+ 
+}
